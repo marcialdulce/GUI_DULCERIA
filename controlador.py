ef13af96baf786f8bbd5b01fedb95fcd47a34c30
Daniel Bailon <danielebm07@gmail.com>
2026-09-05 21:21:32 -0600

    Agregue el nuevo codigo para controlador.py que conecta vista y modelo

---
+++ controlador.py
@@ -0,0 +1,30 @@
+from modelo import ModeloDulceria
+from vista import VistaDulceria
+
+class ControladorDulceria:
+    def __init__(self):
+        self.modelo = ModeloDulceria()
+        self.vista = VistaDulceria(self)
+        self.usuario_actual = None
+
+    def iniciar(self):
+        self.vista.mainloop()
+
+    def procesar_login(self, usuario, password):
+        # Validamos usando el Modelo
+        es_valido = self.modelo.validar_usuario(usuario, password)
+
+        if es_valido:
+            self.usuario_actual = usuario
+            self.vista.mostrar_alerta("Inicio de sesión", f"¡Bienvenido/a, {usuario}!", "info")
+            
+            # Limpiar campos en la vista
+            self.vista.pantallas["PantallaLogin"].txt_usuario.delete(0, 'end')
+            self.vista.pantallas["PantallaLogin"].txt_password.delete(0, 'end')
+            
+            # Actualizar nombre y cambiar pantalla
+            self.vista.pantallas["PantallaPrincipal"].actualizar_usuario(usuario)
+            self.vista.mostrar_pantalla("PantallaPrincipal")
+        else:
+            self.vista.mostrar_alerta("Error", "Usuario o contraseña incorrectos.", "error")
+            
