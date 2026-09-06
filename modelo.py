7bb386bf1db0404c6770d2963f1f0abc57269c82
Daniel Bailon <danielebm07@gmail.com>
2026-09-05 19:45:08 -0600

    Se agrego el codigo para la logia en modelo.py para separar el codigo de gui.py

---
+++ modelo.py
@@ -0,0 +1,19 @@
+class ModeloDulceria:
+
+        def __init__(self):
+                self.usuarios = {
+                        "Dulce" : "1234",
+                        "America" : "1234",
+                        "Daniel" : "1234",
+                        "Yeray" : "1234",
+                        "Josue" : "1234"
+                }
+
+
+        def validar_usuario (self, usuario, password):
+                if usuario in self.usuarios and self.usuarios[usuario] == password:
+                    return True
+                return False
+
+        
+        
