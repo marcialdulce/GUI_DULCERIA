4b660d224111d08a470efed8e323c1752762b60e
Daniel Bailon <danielebm07@gmail.com>
2026-09-05 21:43:23 -0600

    Cambios en vista.py

--- vista.py
+++ vista.py
@@ -33,7 +33,7 @@ class VistaDulceria(tk.Tk):
 
         self.pantallas = {}
         for Pantalla in (PantallaLogin, PantallaPrincipal):
-            frame = Pantalla(parent=self.contenedor, controlador=self.controlador)
+            frame = Pantalla(parent=self.contenedor, controlador=self) 
             self.pantallas[Pantalla.__name__] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
