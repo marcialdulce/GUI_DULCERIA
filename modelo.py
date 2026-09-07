2f90b14c72e2bf3cf1d480e18e59615f241bf847
Daniel Bailon <danielebm07@gmail.com>
2026-09-06 22:15:38 -0600

    Implementa cambio en stock y vista dinámica de Inventario

--- modelo.py
+++ modelo.py
@@ -20,12 +20,11 @@ class ModeloDulceria:
         
         # Definimos exactamente 4 productos con sus datos reales
          self.inventario = [
-            {"nombre": "Gomitas", "precio": 15.0, "stock": 20},
-            {"nombre": "Chocolate", "precio": 12.0, "stock": 15},
-            {"nombre": "Mazapán", "precio": 5.0, "stock": 50},
-            {"nombre": "Panditas", "precio": 20.0, "stock": 10}
+            {"nombre": "Gomitas", "marca": "Ricolino", "precio": 15.0, "stock": 20},
+            {"nombre": "Chocolate", "marca": "Carlos V", "precio": 12.0, "stock": 15},
+            {"nombre": "Mazapán",  "marca": "De la Rosa", "precio": 5.0, "stock": 50},
+            {"nombre": "Panditas",  "marca": "Ricolino", "precio": 20.0, "stock": 10}
         ]
-
 
         # Validación de los usuarios dentro del sistema
         def validar_usuario (self, usuario, password):
@@ -47,16 +46,23 @@ class ModeloDulceria:
         def calcular_total(self):
             return sum(item["subtotal"] for item in self.ticket_actual)
 
-
-
         # Evaluar y calcular el cambio para el ticket
         def procesar_cobro(self, pago_cliente):
             total = self.calcular_total()
             if pago_cliente >= total:
                 cambio = pago_cliente - total
-                self.ticket_actual.clear() # Vaciamos la lista temporal
+
+                # Descontar del stock antes de vaciar el ticket
+                for item_ticket in self.ticket_actual:
+                 for dulce in self.inventario:
+                    if dulce["nombre"] == item_ticket["producto"]:
+                        dulce["stock"] -= item_ticket["cantidad"]
+                        
+                 self.ticket_actual.clear() # Vaciamos la lista temporal
                 return True, cambio
             return False, 0.0
+            
+
 
 
         
