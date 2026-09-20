ab3a5ef6fb340744111a944adadee7031b43e74e
Daniel Bailon <danielebm07@gmail.com>
2026-09-19 21:54:52 -0600

    Elimina texto en main

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     ctrl = controlador.ControladorDulceria() 
     app = VistaDulceria(ctrl)
     ctrl.vista = app
-    app.mainloop()separar vista.py en el paquete vistas/ y organizar MVC
+    app.mainloop()
